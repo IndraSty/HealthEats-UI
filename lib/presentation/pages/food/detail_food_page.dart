9e4aa33bd8e4e995e317b6d4ed7ac994fa8e2a37
Indoradesu <indrastyawan0925@gmail.com>
2024-07-19 14:50:40 +0700

    update detail food page

--- lib/presentation/pages/food/detail_food_page.dart
+++ lib/presentation/pages/food/detail_food_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:healtheats/core/utils/constants.dart';
-import 'package:healtheats/presentation/widgets/circle_button.dart';
+import 'package:healtheats/data/models/food_model.dart';
+import 'package:healtheats/presentation/providers/food_provider.dart';
+import 'package:healtheats/presentation/widgets/button/circle_button.dart';
+import 'package:provider/provider.dart';
 
 class DetailFoodPage extends StatefulWidget {
   final int? id;
@@ -18,149 +21,178 @@ class _DetailFoodPageState extends State<DetailFoodPage> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      // Provider.of<FoodProvider>(context, listen: false)
-      //     .getDetailFood(widget.id);
+      Provider.of<FoodProvider>(context, listen: false)
+          .getDetailFood(widget.id);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    var foods = Foods.allFood;
-    var food = Foods.allFood[0];
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: AppScreenSize.width,
-              height: AppScreenSize.height * 0.35,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(food.image),
-                  fit: BoxFit.cover,
+    return Consumer<FoodProvider>(builder: (context, foodProvider, child) {
+      FoodModel? food = foodProvider.food;
+
+      if (food == null) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+
+      List<String> ingredients = food.ingredients.split('. ');
+      List<String> steps =
+          food.steps.split('_').where((item) => item.isNotEmpty).toList();
+
+      return Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: AppScreenSize.width,
+                height: AppScreenSize.height * 0.35,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(food.image),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 40,
+                      left: 20,
+                      child: CircleButton(
+                        icon: Icons.arrow_back_ios_new_outlined,
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        color: Colors.white,
+                      ),
+                    ),
+                    Positioned(
+                      top: 40,
+                      right: 20,
+                      child: CircleButton(
+                        icon: food.isFavorite
+                            ? Icons.favorite
+                            : Icons.favorite_outline,
+                        onTap: () {},
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
                 ),
               ),
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 40,
-                    left: 20,
-                    child: CircleButton(
-                      icon: Icons.arrow_back_ios_new_outlined,
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      color: Colors.white,
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      food.name,
+                      style: Theme.of(context).textTheme.headlineSmall,
                     ),
-                  ),
-                  Positioned(
-                    top: 40,
-                    right: 20,
-                    child: CircleButton(
-                      icon: Icons.favorite_outline,
-                      onTap: () {},
-                      color: Colors.white,
+                    const SizedBox(
+                      height: 10,
                     ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    food.name,
-                    style: AppTextStyles.poppins_18_bold,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "food.khasiat",
-                    style: AppTextStyles.poppins_12_reguler,
-                    textAlign: TextAlign.justify,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Ingredients',
-                    style: AppTextStyles.poppins_12_reguler,
-                  ),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: foods.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${(index + 1).toString()}. ',
-                              style: AppTextStyles.poppins_14_reguler,
+                    Text(
+                      food.khasiat,
+                      style: Theme.of(context).textTheme.labelLarge,
+                      textAlign: TextAlign.justify,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Ingredients',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    if (ingredients.isNotEmpty)
+                      ListView.builder(
+                        shrinkWrap: true,
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: ingredients.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${(index + 1).toString()}. ',
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    '${ingredients[index]}.',
+                                    style:
+                                        Theme.of(context).textTheme.labelLarge,
+                                  ),
+                                ),
+                              ],
                             ),
-                            const SizedBox(
-                              width: 10,
+                          );
+                        },
+                      )
+                    else
+                      Text(
+                        'No ingredients available',
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Steps',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    if (steps.isNotEmpty)
+                      ListView.builder(
+                        shrinkWrap: true,
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: steps.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${(index + 1).toString()}. ',
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    steps[index],
+                                    style:
+                                        Theme.of(context).textTheme.labelLarge,
+                                  ),
+                                ),
+                              ],
                             ),
-                            Expanded(
-                              child: Text(
-                                '.',
-                                style: AppTextStyles.poppins_12_reguler,
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Steps',
-                    style: AppTextStyles.poppins_18_bold,
-                  ),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: foods.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${(index + 1).toString()}. ',
-                              style: AppTextStyles.poppins_14_reguler,
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Expanded(
-                              child: Text(
-                                "steps[index]",
-                                style: AppTextStyles.poppins_12_reguler,
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
+                          );
+                        },
+                      )
+                    else
+                      Text(
+                        'No steps available',
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
