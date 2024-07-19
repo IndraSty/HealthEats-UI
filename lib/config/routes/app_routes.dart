f89738396f451f0af27bf33b168120c2eb7f7b30
Indoradesu <indrastyawan0925@gmail.com>
2024-07-19 14:47:58 +0700

    update routes

--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:healtheats/presentation/pages/daftar/daftar_page.dart';
+import 'package:healtheats/presentation/pages/diagnosa/result_diagnosa_page.dart';
 import 'package:healtheats/presentation/pages/food/detail_food_page.dart';
 import 'package:healtheats/presentation/pages/login/login_page.dart';
 import 'package:healtheats/presentation/pages/navbar/bottom_nav_page.dart';
+import 'package:healtheats/presentation/pages/profile/my_account_page.dart';
 
 class AppRoutes {
   static const String home = '/';
@@ -12,6 +14,8 @@ class AppRoutes {
   static const String food = '/food';
   static const String detailFood = '/detail-food/:id';
   static const String history = '/history';
+  static const String myAcc = '/my-account';
+  static const String result = '/result-diagnos';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     final args = settings.arguments as Map<String, dynamic>?;
@@ -25,14 +29,20 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => const LoginPage());
       case daftar:
         return MaterialPageRoute(builder: (_) => const DaftarPage());
+      case myAcc:
+        return MaterialPageRoute(builder: (_) => const MyAccountPage());
+      case result:
+        return MaterialPageRoute(builder: (_) => const ResultDiagnosaPage());
       case food:
         return MaterialPageRoute(
             builder: (_) => const BottomNavPage(
                   selectedIndex: 1,
                 ));
-      case detailFood:
-        final id = args?['id'] != null ? int.tryParse(args!['id']) : null;
-        return MaterialPageRoute(builder: (_) => DetailFoodPage(id: id));
+      case AppRoutes.detailFood:
+        final id = args?['id'] as int?;
+        return MaterialPageRoute(
+          builder: (_) => DetailFoodPage(id: id),
+        );
       case profile:
         return MaterialPageRoute(
             builder: (_) => const BottomNavPage(
