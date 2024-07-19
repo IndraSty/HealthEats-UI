0f4645c242b7782f083ad68c3d99374c65ef01e9
Indoradesu <indrastyawan0925@gmail.com>
2024-07-19 14:49:45 +0700

    update main config

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:healtheats/config/routes/app_routes.dart';
+import 'package:healtheats/config/themes/theme.dart';
 import 'package:healtheats/data/repository/auth_repository.dart';
+import 'package:healtheats/data/repository/favorite_food_repository.dart';
 import 'package:healtheats/data/repository/food_repository.dart';
 import 'package:healtheats/data/repository/user_repository.dart';
 import 'package:healtheats/domain/usecases/auth_usecase.dart';
+import 'package:healtheats/domain/usecases/favorite_food_usecase.dart';
 import 'package:healtheats/domain/usecases/food_usecase.dart';
 import 'package:healtheats/domain/usecases/user_usecase.dart';
 import 'package:healtheats/presentation/pages/wrapper/auth_wrapper.dart';
 import 'package:healtheats/presentation/providers/auth_provider.dart';
+import 'package:healtheats/presentation/providers/favorite_food_provider.dart';
 import 'package:healtheats/presentation/providers/food_provider.dart';
 import 'package:healtheats/presentation/providers/user_provider.dart';
 import 'package:provider/provider.dart';
@@ -30,6 +34,10 @@ class MyApp extends StatelessWidget {
     final userUsecase = UserUsecase(userRepository: userRepository);
     final foodRepository = FoodRepositoryImpl(authUsecase: authUsecase);
     final foodUsecase = FoodUsecase(foodRepository: foodRepository);
+    final favoriteFoodRepository =
+        FavoriteFoodRepositoryImpl(authUsecase: authUsecase);
+    final favoriteFoodUsecase =
+        FavoriteFoodUsecase(favoriteFoodRepository: favoriteFoodRepository);
 
     return MultiProvider(
       providers: [
@@ -42,14 +50,17 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (_) => FoodProvider(foodUsecase: foodUsecase),
         ),
+        ChangeNotifierProvider(
+          create: (_) =>
+              FavoriteFoodProvider(favoriteFoodUsecase: favoriteFoodUsecase),
+        ),
       ],
       child: MaterialApp(
         scaffoldMessengerKey: scaffoldMessengerKey,
         title: 'HealthEats',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
+        themeMode: ThemeMode.system,
+        theme: HEAppTheme.ligtTheme,
+        darkTheme: HEAppTheme.darkTheme,
         onGenerateRoute: AppRoutes.generateRoute,
         home: const AuthWrapper(),
       ),
